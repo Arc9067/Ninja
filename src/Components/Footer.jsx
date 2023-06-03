f5fae7e090cc22fb6c9561fb88135f9ce14676b7
Mikail Rokib <atandarokib@gmail.com>
2023-06-03 23:30:37 +0100

    up

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="py-[20px] bg-secondary text-primary">
       <div className="container flex justify-center items-center">
-        <p className="font-mono capitalize">copyright &copy; 2023</p>
+        <p className="font-mono capitalize">copyright Zoonji &copy; 2023</p>
       </div>
     </footer>
   );
